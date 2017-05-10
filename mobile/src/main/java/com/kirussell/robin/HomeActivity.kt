53b36530122b32f392a2e9f38ae10d58aace4be6
kirussell <ruslan.kim@grabtaxi.com>
2017-05-11 00:11:29 +0800

    (FEAT) Using databindings to connect UI

--- mobile/src/main/java/com/kirussell/robin/HomeActivity.kt
+++ mobile/src/main/java/com/kirussell/robin/HomeActivity.kt
@@ -1,21 +1,17 @@
 package com.kirussell.robin
 
 import android.databinding.DataBindingUtil
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.support.v4.app.Fragment
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import com.kirussell.databindings.adapters.ClickObservable
 
-import android.widget.TextView
 import com.kirussell.databindings.adapters.RobinDataBindingComponent
 import com.kirussell.robin.databinding.ActivityHomeBinding
 
@@ -25,17 +21,16 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setDefaultComponent(RobinDataBindingComponent())
         val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
-        binding.viewState = HomeViewState(
+        val homeViewState = HomeViewState(
                 SectionsPagerAdapter(supportFragmentManager)
         )
+        binding.viewState = homeViewState
         setSupportActionBar(binding.toolbar)
-        binding.tabs.setupWithViewPager(binding.container)
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
+        homeViewState.observeFabClick()
+                .subscribe(
+                        { Toast.makeText(applicationContext, "OLOLO", Toast.LENGTH_LONG).show() }
+                )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,46 +50,15 @@ class HomeActivity : AppCompatActivity() {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    class PlaceholderFragment : Fragment() {
-
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            val rootView = inflater!!.inflate(R.layout.fragment_home, container, false)
-            val textView = rootView.findViewById(R.id.section_label) as TextView
-            textView.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
-            return rootView
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private val ARG_SECTION_NUMBER = "section_number"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
-            }
-        }
-    }
-
-    /**
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-        override fun getItem(position: Int) = PlaceholderFragment.newInstance(position + 1)
+        override fun getItem(position: Int) = when (position) {
+            0 -> Fragment()
+            else -> throw IllegalArgumentException("Have no view for position=$position")
+        }
 
         override fun getCount() = 1
 
@@ -105,8 +69,10 @@ class HomeActivity : AppCompatActivity() {
     }
 }
 
-class HomeViewState(
+internal class HomeViewState(
         val adapter: HomeActivity.SectionsPagerAdapter
 ) {
-    fun onFabClick(): () -> kotlin.Unit = {}
+    private val clickObservable = ClickObservable()
+
+    fun observeFabClick() = clickObservable
 }
