8d0dfb47f860c1acdf75e77d263cc43e11ed9882
kirussell <ruslan.kim@grabtaxi.com>
2017-05-11 12:44:26 +0800

    (FEAT) Adds  rx extension function to log onError

---
+++ mobile/src/main/java/com/kirussell/rx/RxExtensions.kt
@@ -0,0 +1,13 @@
+package com.kirussell.rx
+
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.functions.Consumer
+
+fun <T : Throwable> onErrorDefault() : Consumer<T> = Consumer { t ->
+    Log.e("Observable", t.toString())
+}
+
+fun <O> Observable<O>.subscribeNoErr(onNext: (O) -> Unit) {
+    subscribe(Consumer { o -> onNext.invoke(o) }, onErrorDefault<Throwable>())
+}
