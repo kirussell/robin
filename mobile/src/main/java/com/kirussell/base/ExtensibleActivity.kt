e58b27003312932b7420f7ae46e4bcc7e516752c
kirussell <ruslan.kim@grabtaxi.com>
2017-05-15 00:00:45 +0800

    (FEAT) Extensible activity

---
+++ mobile/src/main/java/com/kirussell/base/ExtensibleActivity.kt
@@ -0,0 +1,53 @@
+package com.kirussell.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+open class ExtensibleActivity : AppCompatActivity() {
+
+    val activityLifecycleExtensions = mutableListOf<LifecycleCallbacks>()
+
+    fun addLifecycleCallbacks(callbacks: LifecycleCallbacks) {
+        activityLifecycleExtensions.add(callbacks)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityLifecycleExtensions.forEach { c -> c.onCreate(savedInstanceState) }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        activityLifecycleExtensions.forEach { c -> c.onStart() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activityLifecycleExtensions.forEach { c -> c.onResume() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityLifecycleExtensions.forEach { c -> c.onPause() }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activityLifecycleExtensions.forEach { c -> c.onStop() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activityLifecycleExtensions.forEach { c -> c.onDestroy() }
+    }
+}
+
+interface LifecycleCallbacks {
+    fun onCreate(savedInstanceState: Bundle?): Unit
+    fun onStart(): Unit
+    fun onResume(): Unit
+    fun onPause(): Unit
+    fun onStop(): Unit
+    fun onDestroy(): Unit
+}
