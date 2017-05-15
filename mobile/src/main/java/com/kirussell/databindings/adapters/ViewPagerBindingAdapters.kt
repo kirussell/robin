186fb69c711b777a4bb84ad316dc09e86e88ac42
kirussell <ruslan.kim@grabtaxi.com>
2017-05-16 02:32:22 +0800

    (FEAT) Adds DataBinding adapter for ViewPager for OnPageChangeListener

--- mobile/src/main/java/com/kirussell/databindings/adapters/ViewPagerBindingAdapters.kt
+++ mobile/src/main/java/com/kirussell/databindings/adapters/ViewPagerBindingAdapters.kt
@@ -3,6 +3,8 @@ package com.kirussell.databindings.adapters
 import android.databinding.BindingAdapter
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
 
 class ViewPagerBindingAdapters {
@@ -12,4 +14,25 @@ class ViewPagerBindingAdapters {
         view.adapter = adapter
     }
 
+    @BindingAdapter("android:onPageChangeListener")
+    fun onPageChangeListener(view: ViewPager, changeObs: OnPageChangeObservable) {
+        view.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageSelected(position: Int) {
+                changeObs.acceptSelectedPage(position)
+            }
+        })
+    }
+}
+
+class OnPageChangeObservable {
+
+    val subject: PublishSubject<Int> = PublishSubject.create<Int>()
+
+    internal fun acceptSelectedPage(pagePosition: Int) {
+        subject.onNext(pagePosition)
+    }
+
+    fun observe(): Observable<Int> {
+        return subject
+    }
 }
